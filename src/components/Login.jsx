156ec3d92fffb135109d846e577eae9fb359de2d
trishitchar <trishitchar@gmail.com>
2024-07-21 03:25:15 +0530

    login page style

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,25 +1,51 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import Header from './Header';
 import bg from '../assets/background_flix.jpg';
 
 const Login = () => {
+  const nameRef = useRef(null);
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
+  const [signUp, setSignUp] = useState(false);
+
+  const toggleSignUp = () => {
+    setSignUp(!signUp);
+  };
+
   return (
-    <div className='relative w-full h-screen'>
+    <div className='relative w-full h-screen overflow-hidden'>
       <Header />
       <div className='absolute top-0 left-0 w-full h-full flex justify-center items-center'>
-        <form className='flex justify-center items-center flex-col bg-black bg-opacity-50 p-8 rounded-lg z-10'>
-          <input 
-            type='text' 
-            placeholder='Enter your email' 
-            className='p-2 m-2 border border-gray-300 rounded-lg outline-none w-80' 
+        <form onSubmit={(e) => e.preventDefault()} className='flex flex-col justify-center items-center bg-black bg-opacity-70 p-8 rounded-lg z-10 sm:m-2'>
+          <h1 className="font-bold text-3xl py-4 text-white justify-start">
+            {!signUp ? "Sign In" : "Sign Up"}
+          </h1>
+          {signUp && (
+            <input
+              type='text'
+              ref={nameRef}
+              placeholder='Enter your name'
+              className='p-2 m-2 border border-gray-300 rounded-lg outline-none w-80'
+            />
+          )}
+          <input
+            type='email'
+            ref={emailRef}
+            placeholder='Enter your email'
+            className='p-2 m-2 border border-gray-300 rounded-lg outline-none w-80'
           />
-          <input 
-            type='password' 
-            placeholder='Enter your password' 
-            className='p-2 m-2 border border-gray-300 rounded-lg outline-none w-80' 
+          <input
+            type='password'
+            ref={passwordRef}
+            placeholder='Enter your password'
+            className='p-2 m-2 border border-gray-300 rounded-lg outline-none w-80'
           />
+
           <button className='bg-red-600 text-white m-2 p-2 rounded-md shadow-md w-80 hover:bg-red-700 focus:outline-none'>
-            Sign In
+            {signUp ? "Sign Up" : "Sign In"}
+          </button>
+          <button type='button' onClick={toggleSignUp} className='text-blue-500 hover:underline mt-2'>
+            {!signUp ? "Don't have an account? Sign Up" : "Already have an account? Sign In"}
           </button>
         </form>
       </div>
