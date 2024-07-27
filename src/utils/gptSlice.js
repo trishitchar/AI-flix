44512379508da2c46ab6cffc62c092500cb815ba
trishitchar <trishitchar@gmail.com>
2024-07-28 04:25:08 +0530

    added movienameresult in slice

--- src/utils/gptSlice.js
+++ src/utils/gptSlice.js
@@ -4,13 +4,23 @@ const gptSlice = createSlice({
     name: "gptSlice",
     initialState:{
         gptSearchViewToggle: false,
+        movieName: null,
+        movieResult: null
     },
     reducers:{
         addGptSearchViewToggle: (state,action) =>{
             state.gptSearchViewToggle = !state.gptSearchViewToggle
+        },
+        addGptMovieNameReult : (state,action) =>{
+            state.movieName = action.payload.movieName;
+            state.movieResult = action.payload.movieResult
+        },
+        removeGptMovieNameReult :(state) =>{
+            state.movieName = null;
+            state.movieResult = null;
         }
     }
 })
 
-export const {addGptSearchViewToggle} = gptSlice.actions;
+export const {addGptSearchViewToggle,addGptMovieNameReult,removeGptMovieNameReult} = gptSlice.actions;
 export default gptSlice.reducer;
