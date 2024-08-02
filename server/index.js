8faf29b5991cad7a7d1f19003a15e4dbfb1ffba1
trishitchar <trishitchar@gmail.com>
2024-08-02 12:57:01 +0530

    added some code so server doesn't break

--- server/index.js
+++ server/index.js
@@ -3,20 +3,51 @@ import dotenv from 'dotenv';
 import dbConnection from './utils/db.js';
 import cookieParser from 'cookie-parser';
 import userRoute from './routes/userRoute.js';
+import cors from 'cors';
 
 dotenv.config();
 const app = express();
 
 dbConnection();
 
-app.use(express.json());
+const corsAllowOrigin = {
+  origin: 'http://localhost:5173',
+  credentials: true
+};
+app.use(cors(corsAllowOrigin));
+
+app.use(express.json({ 
+  limit: '10mb', 
+  type: 'application/json' 
+}));
+
 app.use(cookieParser());
+
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/user", userRoute);
 
 app.get('/', (req, res) => {
   res.send('Welcome to the Tchar AIFLIX API');
+});
+
+app.use((err, req, res, next) => {
+  if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
+    console.error('JSON Parsing Error:', err.message);
+    res.status(400).json({ message: "Invalid JSON format", success: false });
+  } else {
+    next(err);
+  }
+});
+
+app.use((err, req, res, next) => {
+  if (err.type === 'stream.not.readable') {
+    console.error('Stream Not Readable Error:', err.message);
+    res.status(500).json({ message: "Stream not readable", success: false });
+  } else {
+    console.error('Server Error:', err);
+    res.status(500).json({ message: "Internal Server Error", success: false });
+  }
 });
 
 const PORT = process.env.PORT || 8080;
