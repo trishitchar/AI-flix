e33baf346ce9ac3654259690cb3eef37a2cab0fe
trishitchar <trishitchar@gmail.com>
2024-08-03 03:40:24 +0530

    render deploy

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const app = express();
 dbConnection();
 
 const corsAllowOrigin = {
-  origin:'http://localhost:5173',
+  origin: ['http://localhost:5173', 'https://ai-flix.onrender.com'],
   // origin:'https://ai-flix.onrender.com',
   credentials: true
 };
